6ce77e3d3badf6476cef059aed33247d45b5d0d3
Gabriel Ong <117062305+gongahkia@users.noreply.github.com>
2023-03-26 23:23:59 +0800

    Update mainInstall.sh

--- installer/mainInstall.sh
+++ installer/mainInstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# --- appends the two neccesary lines to user's .bashrc file
+# --- appends the two neccesary lines to user's .bashrc file ✔️
 echo "export PATH=~/.config/gitfetch-build/bin:$PATH" >> ~/.bashrc
 echo "alias gitfetch='gitfetch.py'" >> ~/.bashrc
 
